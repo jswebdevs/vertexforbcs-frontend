79bb9c9c77c75c092c567bb1289ffb36bab89331
siyamislam000 <tradinglife919@gmail.com>
2025-10-19 19:15:51 +0600

    siyam

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import Routes from './routers/Routes'
 import AdminAuthProvider from './providers/AdminAuthProvider'
 import StudentAuthProvider from './providers/StudentAuthProvider'
+import { RouterProvider } from 'react-router-dom'
+import Routes from './routers/Routes'
 
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AdminAuthProvider>
       <StudentAuthProvider>
-        <RouterProvider router={Routes}></RouterProvider>
+        <RouterProvider router={Routes}>
+        </RouterProvider>
       </StudentAuthProvider>
     </AdminAuthProvider>
-  </StrictMode>
-);
+  </StrictMode>,
+)
