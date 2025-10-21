a16eae5ab52adc204227acd45022aad3d3b13037
jswebdevs <jskeya116@gmail.com>
2025-10-21 23:42:55 +0600

    Updated Footer by JS

--- src/pages/dashboard/Admin/Account/AdminLogin.jsx
+++ src/pages/dashboard/Admin/Account/AdminLogin.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { useAdminAuth } from "../../../../providers/AdminAuthProvider";
 import { useNavigate } from "react-router-dom";
 import { Eye, EyeOff } from "lucide-react"; // 👁️ আইকন যোগ করা হয়েছে
+import Lottie from "lottie-react";
+import loginAnimation from "../../../../assets/login2.json"
 
 const AdminLogin = () => {
   const { login, error, loading } = useAdminAuth();
@@ -22,11 +24,8 @@ const AdminLogin = () => {
   return (
     <div className="hero min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-3xl font-bold">Welcome to Admin Login</h1>
-          <p className="py-6">
-            Please make sure you are logged in before proceeding to the next step.
-          </p>
+        <div className="text-center lg:text-left max-w-sm w-auto">
+          <Lottie animationData={loginAnimation}></Lottie>
         </div>
 
         <div className="card w-full max-w-sm shrink-0 shadow-2xl">
