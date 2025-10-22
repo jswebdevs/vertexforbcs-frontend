fb5c785f80b8f6bb51d7188ba5e3994c1f00f41b
sajidshumik <sajidsoumik394@gmail.com>
2025-10-23 02:36:10 +0600

    shumik4

--- src/pages/dashboard/Students/Account/StudentLogin.jsx
+++ src/pages/dashboard/Students/Account/StudentLogin.jsx
@@ -113,7 +113,7 @@ const StudentLogin = () => {
             {/* Toggle Password */}
             <button
               type="button"
-              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 dark:hover:text-gray-300"
+              className="absolute right-3 top-2/3 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 dark:hover:text-gray-300"
               onClick={() => setShowPassword(!showPassword)}
             >
               {showPassword ? "🙈" : "👁️"}
