ef1ed3d3c04266cc2d0973e0569e4bcf82826b67
jswebdevs <jskeya116@gmail.com>
2025-10-22 12:34:21 +0600

    Updated Firebase

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,23 +1,27 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDliBGg56j4PFkpm9Wnc1VaPnw9ZEoqvxY",
-  authDomain: "vertex-for-bcs-116.firebaseapp.com",
-  projectId: "vertex-for-bcs-116",
-  storageBucket: "vertex-for-bcs-116.firebasestorage.app",
-  messagingSenderId: "960006696417",
-  appId: "1:960006696417:web:5e69eadbfbe2e5919876a0",
-  measurementId: "G-S6G247E2DJ"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const analytics = getAnalytics(app);
 
-export default app;
+const auth = getAuth(app);
+
+export default auth;
