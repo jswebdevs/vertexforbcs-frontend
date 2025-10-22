bed3a4361f15e662ca7da6a09a6a1aefef3af75b
jswebdevs <jskeya116@gmail.com>
2025-10-22 16:31:14 +0600

    Gadhar Dol

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -5,6 +5,7 @@ import SiteFooter from "./SiteFooter";
 const Root = () => {
   return (
     <div className="mx-auto font-poppins max-w-full">
+      <h2 className="text-3xl uppercase">Sob Gadhar dol</h2>
       <SiteHeader></SiteHeader>
       <Outlet></Outlet>
       <SiteFooter></SiteFooter>
